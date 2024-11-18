49ff76745aa19a5988175519993217f49ee1a521
Novan@480s <55051643+Novanz@users.noreply.github.com>
2024-11-18 21:24:32 +0100

    style(City*): Code cleanup/format

--- Blatt6/src/City.cpp
+++ Blatt6/src/City.cpp
@@ -8,56 +8,41 @@
 #include <string>
 
 
-
-
 nan::City::City(const nan::Position &mPosition, std::string *mPOIs, int mNumberOfPOIs)
     : m_position(mPosition), m_number_of_pois(mNumberOfPOIs), m_pois(make_copy(mPOIs, mNumberOfPOIs)) {}
 
 nan::City::City(const std::string &name, int x, int y, std::string *pois, int length)
-    : City(Position(name, x, y),pois, length){}
+    : City(Position(name, x, y), pois, length) {}
 
 nan::City::City(const nan::City &other)
-    : City(other.m_position, other.m_pois, other.m_number_of_pois){}
+    : City(other.m_position, other.m_pois, other.m_number_of_pois) {}
 
 nan::City::~City() {
     delete[] m_pois;
     m_pois = nullptr;
 }
 
-const std::string &nan::City::getName() const {
-    return m_position.getName();
-}
-int nan::City::getX() const {
-    return m_position.getX();
-}
-int nan::City::getY() const {
-    return m_position.getY();
-}
-void nan::City::isValidIndex(int i) const {
-    if (i < 0 || i >= m_number_of_pois) {
-        throw std::out_of_range("Array index out of bounds");
-    }
-}
-
+const std::string &nan::City::getName() const { return m_position.getName(); }
+int nan::City::getX() const { return m_position.getX(); }
+int nan::City::getY() const { return m_position.getY(); }
+void nan::City::isValidIndex(int i) const { if (i < 0 || i >= m_number_of_pois) { throw std::out_of_range("Array index out of bounds"); } }
 
 
 const std::string &nan::City::getPOI(int i) const {
     isValidIndex(i);
     return m_pois[i];
 }
+
 void nan::City::setPOI(int i, const std::string &poi) {
     isValidIndex(i);
     m_pois[i] = poi;
 }
-int nan::City::getNumberOfPOIs() const {
-    return m_number_of_pois;
-}
+
+int nan::City::getNumberOfPOIs() const { return m_number_of_pois; }
 
 void nan::City::addPOI(const std::string &poi) {
     auto *new_pois = new std::string[m_number_of_pois + 1];
-    for (int i = 0; i < m_number_of_pois; i++) {
-        new_pois[i] = m_pois[i];
-    }
+    for (int i = 0; i < m_number_of_pois; i++) { new_pois[i] = m_pois[i]; }
     new_pois[m_number_of_pois] = poi;
     delete[] m_pois;
     // TODO: Frage: muss ich m_pois auf nullptr setzen? oder reicht hier delete[] m_pois da wir es ja gleich neu initialisieren?
@@ -67,19 +52,13 @@ void nan::City::addPOI(const std::string &poi) {
 
 int nan::City::findAndCountPOI(const std::string &poi) const {
     int count = 0;
-    for (int i = 0; i < m_number_of_pois; i++) {
-        if (m_pois[i] == poi) {
-            count++;
-        }
-    }
+    for (int i = 0; i < m_number_of_pois; i++) { if (m_pois[i] == poi) { count++; } }
     return count;
 }
 
 bool nan::City::removePOI(const std::string &poi) {
     int number_of_occurrences = findAndCountPOI(poi);
-    if (number_of_occurrences == 0) {
-        return false;
-    }
+    if (number_of_occurrences == 0) { return false; }
     auto *new_pois = new std::string[m_number_of_pois - number_of_occurrences];
     int j = 0;
     for (int i = 0; i < m_number_of_pois; i++) {
--- Blatt6/src/City.h
+++ Blatt6/src/City.h
@@ -15,14 +15,11 @@ namespace nan {
         nan::Position m_position;
         int m_number_of_pois;
         std::string *m_pois;
+
         static std::string *make_copy(const std::string *source_array, int size) {
-            if (source_array == nullptr && size != 0) {
-                throw std::invalid_argument("Size of nullptr array is not 0");
-            }
+            if (source_array == nullptr && size != 0) { throw std::invalid_argument("Size of nullptr array is not 0"); }
             auto *result = new std::string[size];
-            for (int i = 0; i < size; i++) {
-                result[i] = source_array[i];
-            }
+            for (int i = 0; i < size; i++) { result[i] = source_array[i]; }
             return result;
         }
 
@@ -32,7 +29,7 @@ namespace nan {
         City(const Position &mPosition, std::string *mPOIs, int mNumberOfPOIs);
 
     public:
-        City(const std::string& name, int x, int y, std::string *pois = nullptr, int length = 0);
+        City(const std::string &name, int x, int y, std::string *pois = nullptr, int length = 0);
         City(const nan::City &other);
         ~City();
         const std::string &getName() const;
